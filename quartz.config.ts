05ff9909593e4dcf25b147ce52ee71aaa13408c1
Mikael <ruben.pinas.r@uni.pe>
2026-04-19 23:26:22 -0500

    Quartz sync: Apr 19, 2026, 11:26 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Cursos FIM – Notas personales",
+    pageTitle: "Cursos FIM – con fe y café",
     pageTitleSuffix: "",
     enableSPA: false,
     enablePopovers: true,
